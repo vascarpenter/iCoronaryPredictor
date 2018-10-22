5bd67a5a4fe4baaa92d185410a19cdebe099c3fd
vascarpenter <gikoha5666@gmail.com>
2018-10-22 10:53:46 +0900

    Initial Commit

---
+++ iCoronaryPredictor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iCoronaryPredictor
+//
+//  Created by Namikare Gikoha on 2018/10/22.
+//  Copyright © 2018 gikoha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
